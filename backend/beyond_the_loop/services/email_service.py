00ee4ccab34e527791595a67a2aa842274bcaae6
Phil Szalay <philszalay@aol.de>
2025-11-18 09:24:18 +0100

    Start with loops mail integration

--- backend/beyond_the_loop/services/email_service.py
+++ backend/beyond_the_loop/services/email_service.py
@@ -1,6 +1,4 @@
-from typing import Dict, Optional, List
-import sib_api_v3_sdk
-from sib_api_v3_sdk.rest import ApiException
+from typing import Optional
 from pydantic import EmailStr
 import os
 from pathlib import Path
@@ -8,10 +6,8 @@ from jinja2 import Environment, FileSystemLoader
 
 class EmailService:
     def __init__(self):
-        self.configuration = sib_api_v3_sdk.Configuration()
-        self.configuration.api_key['api-key'] = os.getenv('BREVO_API_KEY')
-        self.api_instance = sib_api_v3_sdk.TransactionalEmailsApi(sib_api_v3_sdk.ApiClient(self.configuration))
-        
+        self.configuration.api_key['api-key'] = os.getenv('LOOPS_API_KEY')
+
         # Initialize Jinja2 environment
         template_dir = Path(__file__).parent.parent / 'templates' / 'email'
         self.jinja_env = Environment(loader=FileSystemLoader(template_dir))
@@ -21,25 +17,18 @@ class EmailService:
         try:
             subject = "Willkommen bei Bchat!"
             sender = {"name": "Bchat", "email": os.getenv('SENDER_EMAIL', 'noreply@beyondtheloop.ai')}
-            to = [{"email": to_email}]
             
             # Load and render the template
             template = self.jinja_env.get_template('invitation-mail.html')
             html_content = template.render(
                 activation_link=os.getenv('BACKEND_ADDRESS') + "/register?inviteToken=" + invite_token,
             )
-            
-            send_smtp_email = sib_api_v3_sdk.SendSmtpEmail(
-                to=to,
-                html_content=html_content,
-                sender=sender,
-                subject=subject
-            )
-            
-            self.api_instance.send_transac_email(send_smtp_email)
+
+
+
             return True
             
-        except ApiException as e:
+        except Exception as e:
             print(f"Exception when sending registration email: {e}")
             return False
 
