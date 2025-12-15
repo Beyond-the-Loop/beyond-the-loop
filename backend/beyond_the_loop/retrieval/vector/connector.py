1160d9115dc2511562da610916d58906185d6ea8
Phil Szalay <philszalay@aol.de>
2025-12-15 09:17:55 +0100

    beyond_the_lopp/retrieval import fixes

--- backend/beyond_the_loop/retrieval/vector/connector.py
+++ backend/beyond_the_loop/retrieval/vector/connector.py
@@ -1,22 +1,3 @@
-from beyond_the_loop.config import VECTOR_DB
+from beyond_the_loop.retrieval.vector.dbs.chroma import ChromaClient
 
-if VECTOR_DB == "milvus":
-    from beyond_the_loop.retrieval.vector.dbs.milvus import MilvusClient
-
-    VECTOR_DB_CLIENT = MilvusClient()
-elif VECTOR_DB == "qdrant":
-    from beyond_the_loop.retrieval.vector.dbs.qdrant import QdrantClient
-
-    VECTOR_DB_CLIENT = QdrantClient()
-elif VECTOR_DB == "opensearch":
-    from beyond_the_loop.retrieval.vector.dbs.opensearch import OpenSearchClient
-
-    VECTOR_DB_CLIENT = OpenSearchClient()
-elif VECTOR_DB == "pgvector":
-    from beyond_the_loop.retrieval.vector.dbs.pgvector import PgvectorClient
-
-    VECTOR_DB_CLIENT = PgvectorClient()
-else:
-    from beyond_the_loop.beyond_the_loop.retrieval.vector.dbs.chroma import ChromaClient
-
-    VECTOR_DB_CLIENT = ChromaClient()
+VECTOR_DB_CLIENT = ChromaClient()
