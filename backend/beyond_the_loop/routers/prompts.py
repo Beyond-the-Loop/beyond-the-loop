e7597698591575115c296556202c2dfbd07c360e
Phil Szalay <philszalay@aol.de>
2026-01-21 10:46:21 +0100

    Prompt creation debugging

--- backend/beyond_the_loop/routers/prompts.py
+++ backend/beyond_the_loop/routers/prompts.py
@@ -98,7 +98,7 @@ async def create_new_prompt(
     if not has_permission(user.id, "workspace.view_prompts"):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail=ERROR_MESSAGES.UNAUTHORIZED,
+            detail=ERROR_MESSAGES.UNAUTHORIZED(),
         )
 
     prompt = Prompts.get_prompt_by_command_and_company(form_data.command, user.company_id)
@@ -110,12 +110,12 @@ async def create_new_prompt(
             return prompt
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail=ERROR_MESSAGES.DEFAULT,
+            detail=ERROR_MESSAGES.DEFAULT(),
         )
 
     raise HTTPException(
         status_code=status.HTTP_400_BAD_REQUEST,
-        detail=ERROR_MESSAGES.COMMAND_TAKEN,
+        detail=ERROR_MESSAGES.COMMAND_TAKEN(),
     )
 
 
